8135a457134b37b0632b44a28ea21b55d3f9563b
Adam SOudani <asoudani@email.com>
2020-07-20 13:45:16 -0700

    button group

---
+++ client/src/components/Buttongroup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Buttongroup() {
+    return (
+        <div>
+            <div className="btn-group" role="group" aria-label="Basic example">
+                <button type="button" className="btn btn-secondary">Left</button>
+                <button type="button" className="btn btn-secondary">Middle</button>
+                <button type="button" className="btn btn-secondary">Right</button>
+            </div>
+        </div>
+    )
+}
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import NavbarDisplay from '../components/NavbarDisplay';
+import Buttongroup from '../components/Buttongroup';
 
 export class Home extends Component {
     render() {
         return (
             <div>
                 <NavbarDisplay />
+                <Buttongroup />
             </div>
         )
     }
