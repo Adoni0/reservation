5084758bcc253128b1bdf9dee2303795120e212f
Adam SOudani <asoudani@email.com>
2020-07-20 12:55:28 -0700

    navbar added

---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import NavbarDisplay from '../components/NavbarDisplay';
+
+export class Home extends Component {
+    render() {
+        return (
+            <div>
+                <NavbarDisplay />
+            </div>
+        )
+    }
+}
+
+export default Home
