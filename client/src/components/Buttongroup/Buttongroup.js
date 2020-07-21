7aa452072973a8753421296b7164f63eba7df554
Adam SOudani <asoudani@email.com>
2020-07-20 23:07:38 -0700

    buttons

---
+++ client/src/components/Buttongroup/Buttongroup.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './style.css';
+
+export function Buttongroup() {
+    return (
+        <div className="row">
+            <div className="col text-center btn-group ml-3">
+                <button type="button" className="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Reserve Lane 1
+  </button>
+                <div className="dropdown-menu">
+                    <a className="dropdown-item" href="#">Action</a>
+                    <a className="dropdown-item" href="#">Another action</a>
+                    <a className="dropdown-item" href="#">Something else here</a>
+                    <div className="dropdown-divider"></div>
+                    <a className="dropdown-item" href="#">Separated link</a>
+                </div>
+            </div>
+
+            <div className="col text-center btn-group ml-3">
+                <button type="button" className="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Reserve Lane 2
+  </button>
+                <div className="dropdown-menu">
+                    <a className="dropdown-item" href="#">Action</a>
+                    <a className="dropdown-item" href="#">Another action</a>
+                    <a className="dropdown-item" href="#">Something else here</a>
+                    <div className="dropdown-divider"></div>
+                    <a className="dropdown-item" href="#">Separated link</a>
+                </div>
+            </div>
+
+            <div className="col text-center btn-group ml-3">
+                <button type="button" className="btn btn-warning dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Reserve Lane 3
+  </button>
+                <div className="dropdown-menu">
+                    <a className="dropdown-item" href="#">Action</a>
+                    <a className="dropdown-item" href="#">Another action</a>
+                    <a className="dropdown-item" href="#">Something else here</a>
+                    <div className="dropdown-divider"></div>
+                    <a className="dropdown-item" href="#">Separated link</a>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export function PoolImage() {
+    return (
+        <img className="pool-image" src="https://images.pexels.com/photos/1263349/pexels-photo-1263349.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
+    )
+}
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import NavbarDisplay from '../components/NavbarDisplay';
-import Buttongroup from '../components/Buttongroup';
+import NavbarDisplay from '../components/NavbarDisplay/NavbarDisplay';
+import { Buttongroup, PoolImage } from '../components/Buttongroup/Buttongroup';
 
 export class Home extends Component {
     render() {
         return (
             <div>
                 <NavbarDisplay />
+               
+                <PoolImage />
                 <Buttongroup />
             </div>
         )
